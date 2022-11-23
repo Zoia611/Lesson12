e34a3a99a3fba6fec7928c5db9df303cf3931436
Zoia611 <117438266+Zoia611@users.noreply.github.com>
2022-11-23 17:03:50 +0300

    Добавьте файлы проекта.

---
+++ Task1/Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Circle
+    {
+        static public double GetLong(double r)
+        {
+            return r * 2 * Math.PI;
+        }
+        static public double GetSquare(double r)
+        {
+            return r * r * Math.PI;
+        }
+        static public string GetCor(double r, double x, double y, double x0 = 0, double y0 = 0)
+        {
+            if (Math.Pow((x - x0), 2) + Math.Pow((y - y0), 2) <= (r * r))
+            {
+                return "принадлежит кругу";
+            }
+            else
+            {
+                return "не принадлежит кругу";
+            }
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Circle
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение радиуса, координаты точки (x,y), а также координаты центра окружности(x0,y0)");
+            double r = Convert.ToDouble(Console.ReadLine());
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            double x0 = Convert.ToDouble(Console.ReadLine());
+            double y0 = Convert.ToDouble(Console.ReadLine());
+
+            double longCircle = Circle.GetLong(r);
+            double square = Circle.GetSquare(r);
+            string conf = Circle.GetCor(r, x, y, x0, y0);
+
+            Console.WriteLine($"Длина окружности: {longCircle:f2}\nПлощадь круга: {square:f2}\nПринадлженость точки: {conf}");
+            Console.ReadLine();
+        }
+    }
+}
